feb15fec84e8edd89c57c0db33d9f24acff0dcdd
fery <panfeng@xiaoxiangyoupin.cn>
2020-04-14 10:19:50 +0800

    remove unused

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import CityPicker from './components/CityPicker.vue';
-import Vue from 'vue'
-console.log(Vue)
+
 export default function install (Vue) {
   if (install.installed) return;
   install.installed = true;
